8b65b87d68384922c9f762fe2ec2a1f1cdcbe728
LeoGes-bit <josue.leon@espoch.edu.ec>
2022-02-01 07:28:53 -0500

    Arquitectura STATE

---
+++ State/Nuevo_Estado.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace State {
+    public class Nuevo_Estado : Estado {
+        public override void ControlarEstado(Switch sw) {
+            sw.DefinirEstado(new Encendido());
+        }
+
+        public override string Describir() {
+            return "SW Nuevo Estado";
+        }
+    }
+}
---
+++ State/StandBy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace State
+{
+    public class StandBy : Estado
+    {
+        public override void ControlarEstado(Switch sw)
+        {
+            sw.DefinirEstado(new Nuevo_Estado());
+        }
+
+        public override string Describir()
+        {
+            return "Switch en stand by";
+        }
+    }
+}
